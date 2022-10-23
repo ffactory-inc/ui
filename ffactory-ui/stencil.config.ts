b908178e87eea5e137f5ed30cf39e8c236594195
Edwin Encinas <edwin.encinas.q@gmail.com>
2022-10-23 15:45:35 -0400

    added react library component

--- ffactory-ui/stencil.config.ts
+++ ffactory-ui/stencil.config.ts
@@ -1,8 +1,14 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget as react } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'ffactory-ui',
   outputTargets: [
+    react({
+      componentCorePackage: 'ffactory-ui-react',
+      proxiesFile: '../ffactory-ui-react/src/components/stencil-generated/index.ts',
+      includeDefineCustomElements: true,
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
